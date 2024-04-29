9d8c49e09af05254df8fed6dbee7ccda6cf2d091
Joelsen <155982256+Joelsen@users.noreply.github.com>
2024-04-29 12:36:13 +0100

    modify login page

--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -40,11 +40,11 @@ const Login = () => {
       <form>
         <div className="rounded-lg flex flex-col border w-[32%] mx-auto mb-10 items-center gap-6 shadow-[2px_2px_10px_2px_rgba(0,0,0,0.2)]">
           <div className="text-[2.125rem] w-full  text-center font-medium pt-4">
-            <p>Login</p>
+            <p className="font-como">Login</p>
           </div>
           <section className="w-[24rem] mb-[10px] flex flex-col gap-8">
             <div className="bg-white w-full pl-4 outline-none focus:border-black valid:border-[#017358]">
-              <h1 className="text-left pb-3 text-[#001712] text-sm font-medium">
+              <h1 className="text-left pb-3 text-[#001712] text-sm font-medium font-como">
                 Email Address
               </h1>
               <input
@@ -56,7 +56,7 @@ const Login = () => {
               />
             </div>
             <div className="bg-white w-full pl-4 outline-none">
-              <h1 className="text-left pb-3 text-[#001712] text-sm font-medium">
+              <h1 className="text-left pb-3 text-[#001712] text-sm font-medium font-como">
                 Password (minimum of 8 characters)
               </h1>
               <div className="flex border border-[#9DA39F] rounded-lg px-2 py-3 items-center  w-full justify-between focus-within:border-black valid:border-[#017358]">
@@ -66,18 +66,18 @@ const Login = () => {
                   placeholder="Enter Password"
                 />
               </div>
-              <p className="pt-2 text-[0.875rem]">
+              <p className="pt-2 text-[0.875rem] font-como">
                 (Use alphabets, numbers and characters)
               </p>
             </div>
             <div className="flex justify-center">
-              <button className="bg-[gold] rounded border w-20 items-center   ">
+              <button className="bg-[#2e3a1a] rounded-full border w-20 items-center  font-como ">
                 Login
               </button>
             </div>
           </section>
           <div className="w-[22rem] mb-[10px] flex flex-col gap-8 text-base font-normal">
-            <h1>
+            <h1 className="font-como">
               Don’t have an account,{" "}
               <Link>
                 {" "}
